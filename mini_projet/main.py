1fbb3292add3ed3bdf73e77632b2088fbe2917aa
snoutmoth <83447836+snoutmoth@users.noreply.github.com>
2021-05-20 14:55:37 +0200

    collision fruit kunai et fruit bombe

--- mini_projet/main.py
+++ mini_projet/main.py
@@ -2,15 +2,22 @@
 # Hello c'est daria - collision du kunai avec fruit et bombe
 
 
+WIDTH = 800
+HEIGHT = 600
+
 kunai = Actor("kunai")
 fruit1 = Actor("fruit1")
 bombe = Actor("bombe")
+joueur = Actor("joueur")
+
+#COLLISION FRUIT + KUNAI ET BOMBE + KUNAI
 
 all_fruits = []
 all_bombs = []
 
 def draw():
 
+    screen.clear()
     if kunai != None:
         kunai.draw()
 
@@ -20,10 +27,15 @@ def draw():
 for fruit1 in all_fruits:
     if kunai.colliderect(fruit1):
         fruit1.remove()
+        kunai.remove()
+        score +=1 
     
 for bombe in all_bombs:
     if kunai.colliderect(bombe):
         bombe.remove()
+        kunai.remove()
+    if bombe.colliderect(joueur):
+        #ici écran de game over/reload ?
 
     
 
