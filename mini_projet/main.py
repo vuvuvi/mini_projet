eae2746ae0c3b11acf373dbe3c95bb7d035c4faf
Reshma Goolamy <com.goolamy@gmail.com>
2021-05-20 15:23:22 +0200

    merger

--- mini_projet/main.py
+++ mini_projet/main.py
@@ -1,17 +1,21 @@
-<<<<<<< HEAD
+
 #Hello c'est marie
 #Hello c'est Reshma 
 
 from random import randint
 
+WIDTH = 800
+HEIGHT = 600
 score = 0
 player = Actor("joueur")
 player.pos = [550, 550]
+kunai = Actor("kunai")
+fruit1 = Actor("fruit1")
+bombe = Actor("bombe")
 
-def draw():
-    screen.clear()
-    player.draw()
-    screen.draw.text(str(score), (550, 590))
+all_fruits = []
+all_bombs = []
+
 
 def on_mouse_move(pos):
     player.pos = [pos[0], player.pos[1]]
@@ -22,28 +26,21 @@ def on_mouse_move(pos):
 
 #compteur de score 
 
-
-=======
 # Hello c'est marie
 # Hello c'est daria - collision du kunai avec fruit et bombe
 
 
-WIDTH = 800
-HEIGHT = 600
 
-kunai = Actor("kunai")
-fruit1 = Actor("fruit1")
-bombe = Actor("bombe")
-joueur = Actor("joueur")
 
 #COLLISION FRUIT + KUNAI ET BOMBE + KUNAI
-
-all_fruits = []
-all_bombs = []
 
 def draw():
 
     screen.clear()
+    player.draw()
+    screen.draw.text(str(score), (550, 590))
+
+    
     if kunai != None:
         kunai.draw()
 
@@ -60,11 +57,4 @@ for bombe in all_bombs:
     if kunai.colliderect(bombe):
         bombe.remove()
         kunai.remove()
-    if bombe.colliderect(joueur):
-        #ici écran de game over/reload ?
-
-    
-
-
-            
->>>>>>> origin/daria
+    #if bombe.colliderect(joueur):
