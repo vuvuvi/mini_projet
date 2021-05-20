cd8ed1a59d97ca614c66628414f70358f33f1df7
VittoriaES <vittoes9@gmail.com>
2021-05-20 15:43:15 +0200

    "kunai move"

--- mini_projet/main.py
+++ mini_projet/main.py
@@ -5,30 +5,32 @@ from random import randint
 WIDTH = 800
 HEIGHT = 600
 
-player = Actor ("joueur")
+player = Actor ("joueur", anchor = ["right", "top"] )
 player.pos = [520, 500]
 
-kunai = Actor ("kunai")
-kunai_speed = [0, 3]
+kunai_speed = [0, - 3]
 all_kunai = []
 
 def draw():
     screen.clear()
     player.draw()
 
+    for kunai in all_kunai:
+        kunai.draw()
+
 def on_mouse_move(pos):
     player.pos = [pos[0], player.pos[1]]
 
-
 def on_mouse_down(pos):
     print ("Click")
-    kunai.draw()
+    kunai = Actor ("kunai", anchor = ["right", "bottom"])
     kunai.pos = player.pos
-
+    all_kunai.append(kunai)
 
 def update(dt):
+    dt += 1
+    for kunai in all_kunai:
+        new_x = kunai.pos [0] + kunai_speed [0]
+        new_y = kunai.pos [1] + kunai_speed [1]
+        kunai.pos = [new_x, new_y]
 
-    new_x = kunai.pos [0] + kunai_speed [0]
-    new_y = kunai.pos [1] + kunai_speed [1]
-
-    kunai.pos = [new_x, new_y]
