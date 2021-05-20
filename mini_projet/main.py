c5d345049158be7cf73e44b6461633e8f206b749
snoutmoth <83447836+snoutmoth@users.noreply.github.com>
2021-05-20 14:29:54 +0200

    collision kunai + fruit

--- mini_projet/main.py
+++ mini_projet/main.py
@@ -1,1 +1,31 @@
 # Hello c'est marie
+# Hello c'est daria - collision du kunai avec fruit et bombe
+
+
+kunai = Actor("kunai")
+fruit1 = Actor("fruit1")
+bombe = Actor("bombe")
+
+all_fruits = []
+all_bombs = []
+
+def draw():
+
+    if kunai != None:
+        kunai.draw()
+
+    if fruit1 != None:
+        fruit1.draw()
+    
+for fruit1 in all_fruits:
+    if kunai.colliderect(fruit1):
+        fruit1.remove()
+    
+for bombe in all_bombs:
+    if kunai.colliderect(bombe):
+        bombe.remove()
+
+    
+
+
+            
