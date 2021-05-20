85a89ef0a065566b2883aba911b4bfea4b8a5311
Marie <fzgaoui@hotmail.fr>
2021-05-20 15:19:53 +0200

    create affichage ecran fond et fruit_score

--- mini_projet/main.py
+++ mini_projet/main.py
@@ -1,4 +1,14 @@
 #Hello c'est marie
+
+#affichage écran
+WIDTH = 800
+HEIGHT = 600
+background = Actor("background")
+background.pos = [400, 300]
+fruit_score = Actor("fruit1_score")
+fruit_score.pos = [720,50]
+
+
 #Hello c'est Reshma 
 
 from random import randint
@@ -9,6 +19,8 @@ player.pos = [550, 550]
 
 def draw():
     screen.clear()
+    background.draw()
+    fruit_score.draw()
     player.draw()
 
 def on_mouse_move(pos):
