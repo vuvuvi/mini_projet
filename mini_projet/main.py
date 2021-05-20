e803f28379775b1425d1320bc24335572b65f987
Reshma Goolamy <com.goolamy@gmail.com>
2021-05-20 15:37:13 +0200

    mise en commun

--- mini_projet/main.py
+++ mini_projet/main.py
@@ -1,21 +1,12 @@
 
-#Hello c'est marie
+from random import randint
 
-#affichage écran
 WIDTH = 800
 HEIGHT = 600
 background = Actor("background")
 background.pos = [400, 300]
 fruit_score = Actor("fruit1_score")
 fruit_score.pos = [720,50]
-
-
-#Hello c'est Reshma 
-
-from random import randint
-
-WIDTH = 800
-HEIGHT = 600
 score = 0
 player = Actor("joueur")
 player.pos = [550, 550]
@@ -31,36 +22,18 @@ def draw():
     background.draw()
     fruit_score.draw()
     player.draw()
-
-def on_mouse_move(pos):
-    player.pos = [pos[0], player.pos[1]]
-
-
-#collide
-
-
-#compteur de score 
-
-# Hello c'est marie
-# Hello c'est daria - collision du kunai avec fruit et bombe
-
-
-
-
-#COLLISION FRUIT + KUNAI ET BOMBE + KUNAI
-
-def draw():
-
-    screen.clear()
-    player.draw()
-    screen.draw.text(str(score), (550, 590))
-
+    screen.draw.text(str(score), (720, 45))
     
     if kunai != None:
         kunai.draw()
 
     if fruit1 != None:
         fruit1.draw()
+
+def on_mouse_move(pos):
+    player.pos = [pos[0], player.pos[1]]
+
+
     
 for fruit1 in all_fruits:
     if kunai.colliderect(fruit1):
