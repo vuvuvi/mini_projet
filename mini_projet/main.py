23456a7c72e23cd5d17aeb21e2250fb392837a42
Reshma Goolamy <com.goolamy@gmail.com>
2021-05-20 15:13:41 +0200

    score

--- mini_projet/main.py
+++ mini_projet/main.py
@@ -10,7 +10,7 @@ player.pos = [550, 550]
 def draw():
     screen.clear()
     player.draw()
-    screen.draw.text(str(score), (20, 20))
+    screen.draw.text(str(score), (550, 590))
 
 def on_mouse_move(pos):
     player.pos = [pos[0], player.pos[1]]
